5c3026b778a14cffd406c5499b5bd364a8280158
Guillaume Fayard <guillaume.fayard@doctolib.com>
2022-03-03 15:19:12 +0100

    Check nonce in callback phase

--- lib/omniauth/strategies/pro_sante_connect.rb
+++ lib/omniauth/strategies/pro_sante_connect.rb
@@ -5,28 +5,35 @@ require 'rack/utils'
 module OmniAuth
   module Strategies
     class ProSanteConnect < OmniAuth::Strategies::OAuth2
+
       option :name, 'pro-sante-connect'
-
       option :authorize_options, [:scope, :acr_values, :response_type]
-
       option :client_options, {
         site: 'https://auth.esw.esante.gouv.fr',
         token_url: '/auth/realms/esante-wallet/protocol/openid-connect/token',
         authorize_url: 'https://wallet.esw.esante.gouv.fr/auth',
       }
+      option :send_nonce, true
 
       info do
         access_token.get('/auth/realms/esante-wallet/protocol/openid-connect/userinfo').parsed
       end
 
       def authorize_params
-        super
-          .merge({nonce: request.params['nonce']}).compact
-          .merge(required_options)
+        add_nonce if options.send_nonce
+        params = super.merge(required_options)
+        persist_nonce if options.send_nonce
+
+        params
       end
 
       def callback_url
         full_host + script_name + callback_path # Do not include query string
+      end
+
+      def callback_phase
+        verify_nonce! if options.send_nonce
+        super
       end
 
       def required_options
@@ -35,6 +42,20 @@ module OmniAuth
           acr_values: 'eidas2',
           response_type: 'code',
         }
+      end
+
+      def add_nonce
+        options.authorize_params[:nonce] = request.params['nonce'] || SecureRandom.hex(24)
+      end
+
+      def persist_nonce
+        session['omniauth.nonce'] = options.authorize_params[:nonce]
+      end
+
+      def verify_nonce!
+        return if request.params['nonce'] == session.delete('omniauth.nonce')
+
+        fail! :invalid_nonce, CallbackError.new(:invalid_nonce, 'Nonce found in id token is invalid')
       end
     end
   end
